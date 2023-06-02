d6a324374c6f6daf988329bae7a3cb8f4b1c7d10
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-06-02 11:55:52 +0200

    adding switch button challenge

---
+++ Java-challenges/js-inputs-and-strings_switch-values/js/index.js
@@ -0,0 +1,36 @@
+/*
+
+Switch the input values of the two input fields when clicking the button
+
+- [X] Add a second input element to the index.html
+- [X] Assign the second input element to a variable
+- [X] Add a third button (a switch button) to the index.html
+- [X] Assign the third button to a variable
+- [X] Make sure that when the switch button is clicked, the values of the two input fields are being switched
+- [X] Make sure that all three buttons work on both input fields
+
+Hint: the HTML entity for the switch button is: &updownarrow
+
+*/
+
+const firstInput = document.querySelector('[data-js="first-input"]');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
+
+uppercaseButton.addEventListener("click", () => {
+  firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
+});
+
+lowercaseButton.addEventListener("click", () => {
+  firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  const switchValue = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = switchValue;
+});
