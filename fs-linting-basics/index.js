a1458b391b074a4a6682b67f1c995e5ce3f695ab
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-06-09 12:15:33 +0200

    adding linting basics

---
+++ fs-linting-basics/index.js
@@ -0,0 +1,15 @@
+function toggleButton() {
+  const cards = document.querySelectorAll('[data-js="card"]');
+
+  cards.forEach(card => {
+    const button = card.querySelector('[data-js="button"]');
+    const link = card.querySelector('[data-js="link"]');
+    const arrow = card.querySelector('[data-js="arrow"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hidden');
+      arrow.classList.toggle('rotate');
+    });
+  });
+}
+
+toggleButton();
