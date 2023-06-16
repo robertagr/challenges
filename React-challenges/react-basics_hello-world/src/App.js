a0ad9362e93d451a5b22bd4e13beab3b2118a180
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-06-16 15:02:32 +0200

    basics react

---
+++ React-challenges/react-basics_hello-world/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <Helloworld></Helloworld>;
+}
+
+function Helloworld() {
+  return <h1>Hello world!</h1>;
+}
