44abbee34c89df26995b1a87a1f065b68140703f
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-06-19 11:18:04 +0200

    changes with second solution

--- React-challenges/react-props_greeting/src/App.js
+++ React-challenges/react-props_greeting/src/App.js
@@ -1,9 +1,21 @@
 import "./styles.css";
 
 export default function App() {
-  return <Greeting name="guys" />;
+  return <Greeting name="Gimena" />;
 }
 
 function Greeting({ name }) {
-  return <h1>Hello {name}</h1>;
+  const coaches = ["Klaus", "Gimena", "Sven"];
+  return <h1>Hello, {coaches.includes(name) ? name : "Stranger"}</h1>;
 }
+
+// SECOND CASE, WE CAN WRITE THE VAR COACHES INSIDE THE FUNCTION APP AND PASS COACHES AS PROPS INSIDE THE GREETING COMPONENT
+
+// export default function App() {
+//   const coaches = ["Klaus", "Gimena", "Sven"];
+//   return <Greeting name="Gimena" coaches={coaches} />;
+// }
+
+// function Greeting({ name, coaches }) {
+//   return <h1>Hello, {coaches.includes(name) ? name : "Stranger"}</h1>;
+// }
