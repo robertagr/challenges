2bb19d47679e6ec0d5393432083233fe90784218
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-06-20 16:38:20 +0200

    adding counter

---
+++ React-challenges/react-state_counter/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  let [count, setCount] = useState(0);
+  const buttonHandlerPlus = () => {
+    setCount(count + 1);
+    console.log("button");
+  };
+  const buttonHandlerMinus = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <div className="button-container">
+        <button type="button" onClick={buttonHandlerMinus}>
+          -
+        </button>
+        <button type="button" onClick={buttonHandlerPlus}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
