722a4a0d11c89fd726fa736583a0e5045409a47a
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-06-21 12:28:28 +0200

    react with arrays users

--- React-challenges/react-with-arrays_users/src/components/Tag.js
+++ React-challenges/react-with-arrays_users/src/components/Tag.js
@@ -2,7 +2,7 @@ import "./Tag.css";
 
 export default function Tag({ tag }) {
   if (tag === "admin") {
-    return <li className="tag--highlight">{tag}</li>;
+    return <li className="tag tag--highlight">{tag}</li>;
   }
   return <li className="tag">{tag}</li>;
 }
