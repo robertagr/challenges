258b65aa532a10236fed2bef087ea2a11213366c
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-06-22 17:24:20 +0200

    iss tracker

---
+++ React-challenges/react-effects-and-fetch_iss-tracker/src/App.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import Controls from "./components/Controls";
+import Map from "./components/Map";
+import "./styles.css";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+export default function App() {
+  const [coords, setCoords] = useState({
+    longitude: 0,
+    latitude: 0,
+  });
+
+  useEffect(() => {
+    const id = setInterval(getISSCoords, 5000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
+  async function getISSCoords() {
+    const response = await fetch(URL);
+    const coords = await response.json();
+    setCoords(coords);
+  }
+
+  return (
+    <main>
+      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Controls
+        longitude={coords.longitude}
+        latitude={coords.latitude}
+        onRefresh={getISSCoords}
+      />
+    </main>
+  );
+}
