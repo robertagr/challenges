16c4da4a38287fef3b288c41ff7243ff61419a22
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-07-07 17:15:42 +0200

    routes.products

--- Backend-challenges/backend-api-routes_products/pages/products/[id].js
+++ Backend-challenges/backend-api-routes_products/pages/products/[id].js
@@ -6,12 +6,16 @@ const fetcher = (url) => fetch(url).then((r) => r.json());
 export default function DetailsPage() {
   const router = useRouter();
   const { id } = router.query;
-  const { data } = useSWR(`/api/products/${id}`, fetcher);
+  const { data, error, isLoading } = useSWR(`/api/products/${id}`, fetcher);
+
+  if (error) return <h1>Error loading data</h1>;
+  if (isLoading) return <h2>Loading...</h2>;
   console.log(data);
+
   return (
     <>
       <h1>Details:</h1>
-      <h1> {data.name}</h1>
+      <p> {data.name}</p>
     </>
   );
 }
